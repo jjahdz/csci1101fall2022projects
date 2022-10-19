6c7f44ff7cae6172cbc87bda4443c456aa49bfcd
jjahdz <89235930+jjahdz@users.noreply.github.com>
2022-10-19 13:35:23 -0500

    final commit

--- hangman/main.py
+++ hangman/main.py
@@ -1,3 +1,4 @@
+from calendar import c
 import re
 
 #Getting the answer.
@@ -19,7 +20,58 @@ for current_answer_character in answer:
 #Game Logic.
 current_incorrect_guesses = 0
 
-while current_incorrect_guesses < num_of_inncorrect_guesses or False not in answer_guessed:
+letters_guessed= []
+
+while current_incorrect_guesses < num_of_inncorrect_guesses and False in answer_guessed:
+    #Game Summary
     print(f"Number of incorrect guesses left: {num_of_inncorrect_guesses-current_incorrect_guesses}")
     
-    #Display Puzzle Board.
+    print("Guessed letters: ", end="")
+
+    for current_guessed_letter in letters_guessed:
+        print(current_guessed_letter, end= " ")
+
+    print()
+    #Display puzzle board.
+    for current_answer_index in range(len(answer)):
+        if answer_guessed[current_answer_index]:
+            print(answer[current_answer_index], end= "")
+        else:
+            print("_",end="")
+
+    print()
+
+    # Let user guess a letter.
+    letter = input("Enter a letter: ")
+
+    letter = letter.upper()
+
+    print()
+
+    #Check if user entered a valid letter.
+    if re.search("^[A-Z]$", letter) and len(letter) == 1 and letter not in letters_guessed:
+        current_letter_index = 0
+
+        #Insert the letter in list of letter guesses (insertion sort)
+
+        for current_letter_guessed in letters_guessed:
+            if letter < current_letter_guessed:
+                break
+
+            current_letter_index += 1
+
+        letters_guessed.insert(current_letter_index, letter)
+
+        # See if letter is in the answer.
+        if letter in answer:
+            for current_answer_index in range(len(answer)):
+                if letter == answer[current_answer_index]:
+                    answer_guessed[current_answer_index] = True
+        else:
+            current_incorrect_guesses += 1
+
+# Post-game summary.
+if current_incorrect_guesses < num_of_inncorrect_guesses:
+    print("Congratulations, you won!")
+else:
+    print(f"Sorry, you lost. The answer was {answer}" )
